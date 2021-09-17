a1dd3be126b212f7c3062ea35de0bcf31f67cf90
Nobody Important <russell@sibipro.com>
2021-09-16 23:47:36 -0700

    master

---
+++ fizzbuzz.cpp
@@ -0,0 +1,33 @@
+//print everyone number but on multiple of 3 say fizz 
+//on multiple of 5 say buzz 
+// multiple of 3 and 5 print fizzbuzz
+
+#include <iostream>
+
+int main()
+{
+    for(int i = 100; i > 0; i--)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                std::cout << "Fizz. \n";
+            }
+
+            else if(i % 5 == 0)
+            {
+                std::cout << "Buzz. \n";
+        
+            }
+            else if(i % 3 == 0)
+            {
+                std::cout << "FizzBuzz. \n";
+            }
+            else
+            {
+                std::cout << i << "\n";
+            }
+        }
+
+
+
+}
